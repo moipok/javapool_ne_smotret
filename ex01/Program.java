4451cf0e40bf0885c3f33c034023dbb2f5e9ad1b
Frodo Barbera <fbarbera@at-n3.msk.21-school.ru>
2021-05-29 14:09:10 +0300

    1

---
+++ ex01/Program.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String args[]) {
+        int number = 479598;
+        Scanner scanner;
+        scanner = new Scanner(System.in);
+        number = scanner.nextInt();
+        System.out.println(number);
+    }
+   
+}
